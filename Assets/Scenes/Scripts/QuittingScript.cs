ee1a7420eafe09ad9c4a97ca594c03a45b92daa5
leondale <62919280+leondale@users.noreply.github.com>
2022-09-20 13:41:16 -0400

    Small changes, 01
    Added a quit button. needs debug statement.
    Added button highlight colors.
    added gamemanager for the quit script.

---
+++ Assets/Scenes/Scripts/QuittingScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuittingScript : MonoBehaviour
+{
+   public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+}
