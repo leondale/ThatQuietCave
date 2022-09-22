4cc413518e9738149ceaac1f9d33d1103b683239
leondale <62919280+leondale@users.noreply.github.com>
2022-09-21 20:55:34 -0400

    trying to fix the null thing
    if you go back home, you can't restart.
    must save changes and move to the computer with two screens so I can  see the results step by step reading the code.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -28,21 +28,25 @@ public class GameManager : MonoBehaviour
 
     StoryBlock currentBlock;
 
-    static StoryBlock block6 = new StoryBlock("Harry goes in a bit deeper. The air gets colder, but soon the draft dies down. He leans back to exhale. he sits relaxed in his thoughts.", "you are dead", "you are dead up");
-    static StoryBlock block5 = new StoryBlock("Harry climbs upon a shelf and proceeds to spark. at long liesurly length, he is dazed and drifting.  He is then shocked when he sees some one walk past him and into the cave.", "wait, then leave.", "follow this person.");
-    static StoryBlock block4 = new StoryBlock("Harry saunters atop the boulder and pulls out his lighter. however, the breeze makes lighting up difficult.", "give up and go home.", "go deeper...", block2, block6);
-    static StoryBlock block3 = new StoryBlock("Forth goes Harry, into the mouth of the cave. He scans for a good place to sit and light up", "go to boulder on left.", "climb to a rock shelf on right", block4, block5);
-    static StoryBlock block2 = new StoryBlock("back you went, on home, no need for adventure", "Quit", "restart", block0, block1);
-    static StoryBlock block1 = new StoryBlock("Young Harry knows about the rumors of treasure in the cave, /n, but Harry just wants a quiet place to smoke this joint.", "Be good. Go Home", "go forth", block2, block3);
-    static StoryBlock block0 = new StoryBlock("Game Over!");
+    static StoryBlock block1000 = new StoryBlock("the code function won't let us point to an incomplete array", "done", "temparary", block10, block10);
+
+    static StoryBlock block60 = new StoryBlock("Harry goes in a bit deeper. The air gets colder, but soon the draft dies down. He leans back to exhale. he sits relaxed in his thoughts.", "you are dead", "you are dead up", block1000, block1000);
+    static StoryBlock block50 = new StoryBlock("Harry climbs upon a shelf and proceeds to spark. at long liesurly length, he is dazed and drifting.  He is then shocked when he sees some one walk past him and into the cave.", "wait, then leave.", "follow this person.", block1000, block1000);
+    static StoryBlock block40 = new StoryBlock("Harry saunters atop the boulder and pulls out his lighter. however, the breeze makes lighting up difficult.", "give up and go home.", "go deeper...", block20, block60);
+    static StoryBlock block30 = new StoryBlock("Forth goes Harry, into the mouth of the cave. He scans for a good place to sit and light up", "go to boulder on left.", "climb to a rock shelf on right", block40, block50);
+    static StoryBlock block20 = new StoryBlock("back you went, on home, no need for adventure", "Quit", "restart", block16, block15);
+    static StoryBlock block15 = new StoryBlock("Young Harry finds himself back at the entrance of the cave, /n, but Harry just wants a quiet place to smoke this joint.", "Be good. Go Home", "go forth", block20, block30);
+    static StoryBlock block10 = new StoryBlock("Young Harry knows about the rumors of treasure in the cave, /n, but Harry just wants a quiet place to smoke this joint.", "Be good. Go Home", "go forth", block20, block30);
+    static StoryBlock block16 = new StoryBlock("Game Over!", "replay", "Quit", block15, block10);
 
     // Start is called before the first frame update
     void Start()
     {
-        DisplayBlock(block1);
+        DisplayBlock(block10);
     }
 
-    void DisplayBlock(StoryBlock block) {
+    void DisplayBlock(StoryBlock block) 
+    {
         MainText.text = block.story;
         option1.GetComponentInChildren<Text>().text = block.option1Text;
         option2.GetComponentInChildren<Text>().text = block.option2Text;
